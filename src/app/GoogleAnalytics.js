ef818367031ca23dc4835a864972005386fab736
Debarjun <debarjunthakur020@gmail.com>
2024-05-22 21:38:50 +0530

    Added Google Analytics and HotJar

---
+++ src/app/GoogleAnalytics.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Script from 'next/script';
+
+const GoogleAnalytics = () => {
+  return (
+    <>
+      <Script
+        strategy='lazyOnload'
+        src={`https://www.googletagmanager.com/gtag/js?id=G-HDBMKXWL0D`}
+      />
+
+      <Script id='' strategy='lazyOnload'>
+        {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-HDBMKXWL0D', {
+              page_path: window.location.pathname,
+              });
+          `}
+      </Script>
+    </>
+  );
+};
+
+export default GoogleAnalytics;
