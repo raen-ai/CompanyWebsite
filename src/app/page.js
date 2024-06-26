cb7df84e6f40f00d2308b7b9f2857a2aab71dadc
Debarjun Thakur <107988104+Itshardtofindagoodname@users.noreply.github.com>
2024-06-26 14:43:20 +0530

    Added the Qdrant Logo (#10)
    * Added Logo
    
    * Updated the Logo
    
    * Added logo
    
    * Updated Logo
    
    * Update README.md
    
    * Fixed bugs
    
    * Removed Blurry Logo
    
    * Added Google Analytics and HotJar
    
    * Update README.md
    
    * Added Testimonial
    
    * Added Second Testimony
    
    * Added Qdrant logo to the page
    
    * Added Ordo logo
    
    * Update README.md
    
    ---------
    
    Co-authored-by: Abis <63680552+SirL0gic@users.noreply.github.com>

--- src/app/page.js
+++ src/app/page.js
@@ -8,12 +8,12 @@ import WhatWeDo from "@/components/WhatWedo";
 import { useState, useRef, useEffect } from "react";
 import Cookies from "js-cookie";
 import "toastr/build/toastr.min.css";
+import Qdrant from "../partners/qdrant.png";
 import Skillbuzz from "../partners/skillbuzz.png";
-import Spotify from "../assets/spotify.png";
 import Swift from "../partners/swift.png";
 import Trussme from "../partners/trussme.png";
 import Unipreneur from "../partners/unipreneur.png";
-import Adobe from "../assets/Adobe.png";
+import Ordo from "../partners/ordo.png";
 import Image from "next/image";
 import Vector from "../assets/vectorHero.svg";
 import VectorStart from "../assets/Vector.png";
@@ -348,10 +348,10 @@ export default function Home() {
         </div>
         <div className="cli-resp-trusted-partner flex flex-wrap md:flex-nowrap lg:flex-wrap justify-center md:justify-between gap-5">
           <div>
-            <Image src={Swift} className="opacity-25 h-[48px] w-[129px]" alt="img" draggable="false"/>
+            <Image src={Qdrant} className="opacity-25 h-[48px] w-[129px]" alt="img" draggable="false"/>
           </div>
           <div>
-            <Image src={Spotify} className="opacity-25 h-[48px] w-[129px]" alt="img" draggable="false"/>
+            <Image src={Swift} className="opacity-25 h-[48px] w-[129px]" alt="img" draggable="false"/>
           </div>
           <div>
             <Image src={Skillbuzz} className="opacity-25 h-[48px] w-[129px]" alt="img" draggable="false"/>
@@ -360,7 +360,7 @@ export default function Home() {
             <Image src={Trussme} className="opacity-25 h-[48px] w-[129px]" alt="img" draggable="false"/>
           </div>
           <div>
-            <Image src={Adobe} className="opacity-25 h-[48px] w-[129px]" alt="img" draggable="false"/>
+            <Image src={Ordo} className="opacity-25 h-[48px] w-[129px]" alt="img" draggable="false"/>
           </div>
           <div>
             <Image src={Unipreneur} className="opacity-25 h-[48px] w-[129px]" alt="img" draggable="false"/>
